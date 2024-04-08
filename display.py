aa80a99572a5a82bcc056e15f4f8611568ae2d60
leeal <leeal111@163.com>
2024-04-08 15:31:57 +0800

    更新了plt画图相关实现

--- display.py
+++ display.py
@@ -3,9 +3,15 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
+plt.rcParams["font.sans-serif"] = [
+    "SimHei",
+]  # 设置中文字体族
+
+fig_size = (8, 6)
+font_size = 20
+
 
 def normalize_img(data):
-    # USage：对于任意数据的np数组，归一化到0-255区域。已经在0-255则仅转换为np.uint8
 
     ma = np.max(data)
     mi = np.min(data)
@@ -24,51 +30,51 @@ def normalize_img(data):
     return img
 
 
-def line_chart_img(list, x_label=None):
-    # USage：折线图图像数据
+def plt_fig_img(fig):
+    fig.canvas.draw()
+    image_array = np.array(fig.canvas.renderer.buffer_rgba())
+    bgr_image = cv2.cvtColor(image_array, cv2.COLOR_RGBA2BGR)
+    plt.close(fig)
+    return bgr_image
 
-    x = range(len(list))
-    if x_label != None:
-        plt.xticks(x, x_label)
-    plt.plot(x, list)
-    max_index = np.argmax(list)
-    max_value = list[max_index]
+
+def line_chart_img(y, x=None):
+    if x is None:
+        x = range(len(y))
+    fig, ax = plt.subplots(figsize=fig_size)
+    plot_line(y, x, ax, name="1")
+    return plt_fig_img(fig)
+
+
+def plot_line(y, x, ax, name):
+    ax.plot(x, y, label=name)
+    max_index = np.argmax(y)
+    max_value = y[max_index]
+    y_range = ax.get_ylim()
     plt.annotate(
-        f"           ({max_value:.2f})",
+        f"(index:{max_index} value:{max_value:.1e})",
         xy=(x[max_index], max_value),
-        xytext=(x[max_index], max_value),
+        xytext=(x[max_index], max_value + (y_range[1] - y_range[0]) / 50),
         arrowprops=dict(facecolor="black", arrowstyle="->"),
     )
-    fig = plt.gcf()
-    fig.canvas.draw()
-    rgba_img = np.array(fig.canvas.renderer.buffer_rgba())
-    plt.clf()
-    return cv2.cvtColor(rgba_img, cv2.COLOR_RGB2BGR)
 
 
 def tile_matrix_img(imgs):
-    # USage：显示一个图像矩阵或者返回拼接好的图像矩阵
-
-    return normalize_img(np.vstack([np.hstack(x) for x in imgs]), True)
+    return np.vstack([np.hstack(x) for x in imgs])
 
 
-def add_angle_img(img, angle, noPrintNum=0):
-    #  返回带角度线的图
-
+def add_angle_img(img, angle, central_not_print_num=0):
     maxr = min(img.shape) // 2
     h = img.shape[0] // 2
     w = img.shape[1] // 2
     img_c = np.copy(img)
     for r in range(maxr):
-        if r < noPrintNum:
+        if r < central_not_print_num:
             continue
         img_c[
             h + int(r * math.sin(angle / 360 * 2 * math.pi)),
             w + int(r * math.cos(angle / 360 * 2 * math.pi)),
         ] = 255
-    for r in range(maxr):
-        if r < noPrintNum:
-            continue
         img_c[
             h - int(r * math.sin(angle / 360 * 2 * math.pi)),
             w - int(r * math.cos(angle / 360 * 2 * math.pi)),
@@ -77,79 +83,56 @@ def add_angle_img(img, angle, noPrintNum=0):
     return img_c
 
 
+titles = {
+    "v1_list_score": "平稳程度",
+    "signal_peek_value_list_score": "总体能量值",
+    "signal_noise_radio_list_score": "峰值信噪比",
+    "nn_ifftimg_result": "神经网络",
+    "svm_list_result": "支持向量机",
+}
+
+
 def roc_img(name, fpr, tpr, auc_score):
-    titles = {
-        "v1_list_score": "平稳程度",
-        "signal_peek_value_list_score": "总体能量值",
-        "signal_noise_radio_list_score": "峰值信噪比",
-        "nn_ifftimg_result": "神经网络",
-        "svm_list_result": "支持向量机",
-    }
-    plt.figure()
-    plt.rcParams["font.sans-serif"] = ["SimHei"]
-    plt.plot(fpr, tpr, label="ROC curve (mean precision = %0.2f)" % auc_score)
-    plt.plot([0, 1], [0, 1], "k--")  # 绘制对角线
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.0])
-    plt.xlabel("假阳率", fontsize=20)
-    plt.ylabel("真阳率", fontsize=20)
-    plt.title(titles[name], fontsize=20)
-    plt.legend(loc="lower right", fontsize=20)
-    fig = plt.gcf()
-    fig.canvas.draw()
-    rgba_img = np.array(fig.canvas.renderer.buffer_rgba())
-    plt.clf()
-    return cv2.cvtColor(rgba_img, cv2.COLOR_RGB2BGR)
+    fig, ax = plt.subplots(figsize=fig_size)
+    ax.plot(fpr, tpr, label=f"ROC curve (mean precision = {auc_score:0.2f}%)")
+    ax.plot([0, 1], [0, 1], "k--")
+    ax.xlim([0.0, 1.0])
+    ax.ylim([0.0, 1.0])
+    ax.xlabel("假阳率", fontsize=font_size)
+    ax.ylabel("真阳率", fontsize=font_size)
+    ax.title(titles[name], fontsize=font_size)
+    ax.legend(loc="lower right", fontsize=font_size)
+    return plt_fig_img(fig)
 
 
 def pr_img(name, precision, recall, f1_score):
-    titles = {
-        "v1_list_score": "平稳程度",
-        "signal_peek_value_list_score": "总体能量值",
-        "signal_noise_radio_list_score": "峰值信噪比",
-        "nn_ifftimg_result": "神经网络",
-        "svm_list_result": "支持向量机",
-    }
-    plt.figure()
-    plt.rcParams["font.sans-serif"] = ["SimHei"]
-    plt.plot(recall, precision, label="PR curve (AP = %0.2f)" % f1_score)
-    plt.plot([1, 0], [0, 1], "k--")  # 绘制对角线
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.0])
-    plt.xlabel("Recall", fontsize=20)
-    plt.ylabel("Precision", fontsize=20)
-    plt.title(titles[name], fontsize=20)
-    plt.legend(loc="lower right", fontsize=20)
-    fig = plt.gcf()
-    fig.canvas.draw()
-    rgba_img = np.array(fig.canvas.renderer.buffer_rgba())
-    plt.clf()
-    return cv2.cvtColor(rgba_img, cv2.COLOR_RGB2BGR)
+    fig, ax = plt.subplots(figsize=fig_size)
+    ax.plot(recall, precision, label=f"PR curve (AP = {f1_score:0.2f})")
+    ax.plot([1, 0], [0, 1], "k--")
+    ax.xlim([0.0, 1.0])
+    ax.ylim([0.0, 1.0])
+    ax.xlabel("Recall", fontsize=font_size)
+    ax.ylabel("Precision", fontsize=font_size)
+    ax.title(titles[name], fontsize=font_size)
+    ax.legend(loc="lower right", fontsize=font_size)
+    return plt_fig_img(fig)
 
 
-def lines_chart_img(lists, names, x_label=None):
-    # USage：折线图图像数据
+def lines_chart_img(ys, names, x=None):
+    if x is None:
+        x = range(len(ys[0]))
+    fig, ax = plt.subplots(figsize=fig_size)
+    for i, y in enumerate(ys):
+        plot_line(y, x, ax, names[i])
+    ax.legend()
+    return plt_fig_img(fig)
 
-    x = range(len(lists[0]))
-    if x_label != None:
-        plt.xticks(x, x_label)
-    for i, l in enumerate(lists):
-        plt.plot(x, l, label=names[i])
-        max_index = np.argmax(l)
-        max_value = l[max_index]
-        plt.annotate(
-            f"           ({max_value:.2f})",
-            xy=(x[max_index], max_value),
-            xytext=(x[max_index], max_value),
-            arrowprops=dict(facecolor="black", arrowstyle="->"),
-        )
-    # plt.xlabel("Recall", fontsize=20)
-    # plt.ylabel("Precision", fontsize=20)
-    # plt.title(titles[name], fontsize=20)
-    plt.legend()
-    plt.show()
-    fig = plt.gcf()
-    fig.canvas.draw()
-    rgba_img = np.array(fig.canvas.renderer.buffer_rgba())
-    plt.clf()
-    return cv2.cvtColor(rgba_img, cv2.COLOR_RGB2BGR)
+
+if __name__ == "__main__":
+    y = [5, 1, 4, 2, 3]
+    y2 = [1, 2, 3, 4, 5]
+    img = lines_chart_img([y, y2], ["1", "2"])
+    imgs = [[img, img], [img, img]]
+
+    cv2.imshow("test", img)
+    cv2.waitKey(0)
